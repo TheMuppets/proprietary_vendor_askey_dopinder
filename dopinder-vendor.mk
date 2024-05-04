c543e821952eef016e7c258f279dac165b220fab
Nolen Johnson <johnsonnolen@gmail.com>
2024-05-04 01:49:06 -0400

    Import IR remote mapping and standardize
    * Drop other ones as they're now in device/lineage/atv.
    
    Change-Id: I0ebe561536e4174fdd8becf8a6ded67f0291b2b5

--- dopinder-vendor.mk
+++ dopinder-vendor.mk
@@ -16,25 +16,4 @@ PRODUCT_COPY_FILES += \
     vendor/askey/dopinder/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta \
     vendor/askey/dopinder/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c5ca.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c5ca.ta \
     vendor/askey/dopinder/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_0508_Product_0110.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0508_Product_0110.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_0957_Product_0004.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0957_Product_0004.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_1d5a_Product_c082.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_1d5a_Product_c082.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_248a_Product_8266.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_248a_Product_8266.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_7545_Product_0180.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_7545_Product_0180.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_000d_Product_3838.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_000d_Product_3838.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_000d_Product_3839.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_000d_Product_3839.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_005d_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_005d_Product_0001.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_005d_Product_0002.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_005d_Product_0002.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0484_Product_5738.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0484_Product_5738.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0508_Product_0110.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0508_Product_0110.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0957_Product_0004.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0957_Product_0004.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0c45_Product_1109.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0c45_Product_1109.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_1915_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1915_Product_0001.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_1d5a_Product_c081.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1d5a_Product_c081.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_1d5a_Product_c082.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1d5a_Product_c082.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_248a_Product_8266.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_248a_Product_8266.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_7045_Product_1820.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_7045_Product_1820.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_7545_Product_0021.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_7545_Product_0021.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_7545_Product_0180.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_7545_Product_0180.kl
+    vendor/askey/dopinder/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta
